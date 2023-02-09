1c2656fe102e76d13f90ea2a160aed4c7bb080b1
deepaklearner <deepaklearner@gmail.com>
2023-02-08 20:29:50 -0500

    updated

--- create_story.py
+++ create_story.py
@@ -1,12 +1,11 @@
 from pyral import Rally
 import sys
 
-def create_defect(name, description, state, sstate):
+def create_defect(project, name, description, state, sstate):
 
     RALLY_NAME = 'rally1.rallydev.com'
     RALLY_API_KEY = '_1iKyJD6XRoakADK4LwWSuDsuW0TtCEJupM7z4xbd85g'
 
-    project = 'Project_Test'
     rally = Rally(RALLY_NAME, apikey=RALLY_API_KEY, workspace='Workspace 1', project=project)
 
     proj = rally.getProject()
@@ -30,4 +29,4 @@ def create_defect(name, description, state, sstate):
         sys.exit(1)
     print("Defect created, ObjectID: %s  FormattedID: %s" % (defect.oid, defect.FormattedID))
 
-create_defect(name="Title 1", description = "Description 1",state = "Open",sstate = "Defined")
+create_defect(project = 'Project_Test', name="Title 1", description = "Description 1", state = "Open",sstate = "Defined")
