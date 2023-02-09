33519507012b1a2fb480829fb8d75f01716aacd5
deepaklearner <deepaklearner@gmail.com>
2023-02-08 20:37:27 -0500

    updated

--- create_story.py
+++ create_story.py
@@ -1,7 +1,7 @@
 from pyral import Rally
 import sys
 
-def create_defect(rally_project, rally_title, rally_description, rally_state, rally_sstate):
+def create_defect(rally_project, rally_defect_title, rally_defect_description, rally_defect_state, rally_defect_sstate):
 
     RALLY_NAME = 'rally1.rallydev.com'
     RALLY_API_KEY = '_1iKyJD6XRoakADK4LwWSuDsuW0TtCEJupM7z4xbd85g'
@@ -13,14 +13,11 @@ def create_defect(rally_project, rally_title, rally_description, rally_state, ra
     # get the first (and hopefully only) user whose DisplayName is 'Sartorious Submitter'
     user = rally.getUserInfo(name='deepak raushan').pop(0)
 
-    # Mapping details #
-    # SNOW
-
     defect_data = { "Project" : proj.ref,
-                    "State" : rally_state,
-                    "ScheduleState" : rally_sstate,
-                    "Name" : rally_title,
-                    "Description" : rally_description}
+                    "State" : rally_defect_state,
+                    "ScheduleState" : rally_defect_sstate,
+                    "Name" : rally_defect_title,
+                    "Description" : rally_defect_description}
 
     try:
         defect = rally.create('Defect', defect_data)
@@ -29,4 +26,9 @@ def create_defect(rally_project, rally_title, rally_description, rally_state, ra
         sys.exit(1)
     print("Defect created, ObjectID: %s  FormattedID: %s" % (defect.oid, defect.FormattedID))
 
-create_defect(rally_project = 'Project_Test', rally__title="Title 1", rally_description = "Description 1", rally_state = "Open",rally_sstate = "Defined")
+# Mapping details
+snow_assignmentgrp = "DEEPAK-PROD-SUPPORT"
+snow_problem_sh_desc =
+
+create_defect(rally_project = 'Project_Test', rally_defect_title="Title 1", rally_defect_description = "Description 1",
+              rally_defect_state = "Open",rally_defect_sstate = "Defined")
